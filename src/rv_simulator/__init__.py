317bca6514e797794b8d63c193625d4d58684f03
Arkaprova Dutta <72149477+dutta-arka@users.noreply.github.com>
2025-08-15 12:29:44 +0530

    Create __init__.py

---
+++ src/rv_simulator/__init__.py
@@ -0,0 +1,23 @@
+"""
+RV-Simulator: A package for simulating radial velocity observations
+"""
+
+__version__ = "0.1.0"
+__author__ = "Arkaprova Dutta"
+
+# Core imports for easy access
+from .core.keplerian import simulate_planetary_system
+from .core.convolution import convolve_IP
+from .core.doppler import apply_rv_shift
+from .io.spectrum_reader import read_spectrum_csv, read_FTS_fits
+from .io.fits_handler import write_default_fits, slice_orders
+
+__all__ = [
+    'simulate_planetary_system',
+    'convolve_IP', 
+    'apply_rv_shift',
+    'read_spectrum_csv',
+    'read_FTS_fits',
+    'write_default_fits',
+    'slice_orders'
+]
