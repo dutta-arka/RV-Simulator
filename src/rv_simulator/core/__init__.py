ab3c5c296a24671c96ba8b1e17bcbcb40315ecbb
Arkaprova Dutta <72149477+dutta-arka@users.noreply.github.com>
2025-10-14 21:53:10 +0530

    Update __init__.py

--- src/rv_simulator/core/__init__.py
+++ src/rv_simulator/core/__init__.py
@@ -1,12 +1,13 @@
 """Core functionality for RV simulation"""
 
 from .keplerian import simulate_planetary_system
-from .convolution import convolve_IP, _bigaussian_kernel, _voigt_kernel
+from .convolution import convolve_IP, _bigaussian_kernel, _voigt_kernel, simulate_stellar_rv
 from .doppler import apply_rv_shift
 
 __all__ = [
     'simulate_planetary_system',
-    'convolve_IP', 
+    'convolve_IP',
+    'simulate_stellar_rv',
     '_bigaussian_kernel',
     '_voigt_kernel',
     'apply_rv_shift'
