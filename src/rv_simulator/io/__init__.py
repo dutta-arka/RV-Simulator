d568ecc2ce93f62b453a77e2b3b14b2c0763a8ad
Arkaprova Dutta <72149477+dutta-arka@users.noreply.github.com>
2025-10-14 22:12:51 +0530

    Update __init__.py

--- src/rv_simulator/io/__init__.py
+++ src/rv_simulator/io/__init__.py
@@ -1,11 +1,24 @@
 """Input/Output functionality"""
 
-from .spectrum_reader import read_spectrum_csv, read_FTS_fits
-from .fits_handler import write_default_fits, slice_orders
+# from .spectrum_reader import read_spectrum_csv, read_FTS_fits
+# from .fits_handler import write_default_fits, slice_orders
+
+# __all__ = [
+#     'read_spectrum_csv',
+#     'read_FTS_fits', 
+#     'write_default_fits',
+#     'slice_orders'
+# ]
+
+from .spectrum_reader import read_spectrum_csv, read_FTS_fits, read_FTS_fits_auto
+from .fits_handler import write_default_fits, slice_orders, load_original_orders, auto_detect_orders_from_fits
 
 __all__ = [
     'read_spectrum_csv',
-    'read_FTS_fits', 
+    'read_FTS_fits',
+    'read_FTS_fits_auto',
     'write_default_fits',
-    'slice_orders'
+    'slice_orders',
+    'load_original_orders',
+    'auto_detect_orders_from_fits',
 ]
