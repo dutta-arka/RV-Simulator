a97fbddf42da84640db4b7623c4a686bebf1b216
Arkaprova Dutta <72149477+dutta-arka@users.noreply.github.com>
2025-10-27 12:56:34 +0530

    Update constants.py

--- src/rv_simulator/utils/constants.py
+++ src/rv_simulator/utils/constants.py
@@ -5,6 +5,7 @@ Physical and astronomical constants used in RV simulations
 # Physical constants
 SPEED_OF_LIGHT = 299792458.0  # m/s
 G = 6.67430e-11               # m^3 kg^-1 s^-2
+VIPER_IP_HALF_SIZE = 50
 
 # Astronomical constants  
 M_SUN = 1.98847e30    # kg
